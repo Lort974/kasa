c569c80a1a09055eab852f0ee1d865c3d5fdcfc8
Laurent Bigey <laurent.bigey@outlook.fr>
2024-02-12 08:42:21 +0100

    corrections critères

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,10 +3,12 @@ import housings from '../datas/logements.json'
 
 const Cards = () => {
     const cardsList = housings.map(housing => (
-        <NavLink className="cardList__card" key={housing.id} to={"housings/"+housing.id}>
-            <div className='cardList__card__title'>{housing.title}</div>
-            <img className='cardList__card__img' src={housing.cover} alt="Illustration du logement" />
-        </NavLink>
+        <article key={housing.id} className="cardList__card">
+            <NavLink className="cardList__card__link" to={"housings/"+housing.id}>
+                <div className='cardList__card__link__title'>{housing.title}</div>
+                <img className='cardList__card__link__img' src={housing.cover} alt="Illustration du logement" />
+            </NavLink>
+        </article>
     ))
 
 
